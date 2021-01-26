d55afaab3fdb2362dc9a8b408905631cc0edcaa7
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-01-25 22:54:25 -0600

    functionality achieved

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const validator = require("email-validator");
+const emailValidator = validator.validate(this.email)
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -28,6 +31,7 @@ const questions = [
     },
     {
         name: "email",
+        type: "input",
         message: "What is their employee email address?",
     },
     {
@@ -47,7 +51,7 @@ const questions = [
     },
     {
         name: "anotherMember",
-        message: "Would you like to add another team memeber?",
+        message: "Would you like to add another team member?",
         type: "confirm"
     }
     
@@ -55,7 +59,23 @@ const questions = [
 const gatherTeam = () => {
     inquirer.prompt(questions)
     .then((answers) => {
-        console.log(answers)
+        if (answers.role === "Manager") {
+            let employee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            employees.push(employee)
+        }if (answers.role === "Engineer") {
+            let employee = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            employees.push(employee)
+        }if (answers.role === "Intern") {
+            let employee = new Intern(answers.name, answers.id, answers.email, answers.school);
+            employees.push(employee)
+        }if (answers.anotherMember === true){
+            gatherTeam()
+        }else {
+            fs.writeFile( 'index.html', render(employees), function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+              })
+        }
     })
 }
 gatherTeam()
